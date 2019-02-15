e6a7f7277b20cf21dbe7ae4b43f8b9be091e9fe8
Michail Bondarenko <mcmikius15@gmail.com>
2019-02-15 14:56:48 +0200

    Create two models users and task

---
+++ ToDoFire/ToDoFire/Models/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  ToDoFire
+//
+//  Created by Michail Bondarenko on 2/15/19.
+//  Copyright © 2019 Michail Bondarenko. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+struct Task {
+    let title: String
+    let userId: String
+    let ref: DatabaseReference?
+    var completed: Bool = false
+    
+    init(title: String, userId: String) {
+        self.title = title
+        self.userId = userId
+        self.ref = nil
+    }
+    init(snapshot: DataSnapshot) {
+        let snapshotValue = snapshot.value as! [String: AnyObject]
+        title = snapshotValue["title"] as! String
+        userId = snapshotValue["userId"] as! String
+        completed = snapshotValue["completed"] as! Bool
+        ref = snapshot.ref
+    }
+}
---
+++ ToDoFire/ToDoFire/Models/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  ToDoFire
+//
+//  Created by Michail Bondarenko on 2/15/19.
+//  Copyright © 2019 Michail Bondarenko. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+struct User {
+    let uid: String
+    let email: String
+    
+    init(user: User) {
+        self.uid = user.uid
+        self.email = user.email
+    }
+}

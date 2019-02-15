b9c9779e5c0e138c8ef6a75307d82b92287c7ff5
Michail Bondarenko <mcmikius15@gmail.com>
2019-02-15 09:15:45 +0200

    Add tableView controller and create outlets and action

---
+++ ToDoFire/ToDoFire/Controller/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ToDoFire
+//
+//  Created by Michail Bondarenko on 2/15/19.
+//  Copyright © 2019 Michail Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+    
+
+}
+
---
+++ ToDoFire/ToDoFire/Controller/TasksViewController.swift
@@ -0,0 +1,54 @@
+//
+//  TasksViewController.swift
+//  ToDoFire
+//
+//  Created by Michail Bondarenko on 2/15/19.
+//  Copyright © 2019 Michail Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = UIColor.clear
+        cell.textLabel?.text = "This is cell number \(indexPath.row)"
+        cell.textLabel?.textColor = UIColor.white
+        return cell
+    }
+    
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
